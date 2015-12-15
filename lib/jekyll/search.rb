44abeb689b1a3c55483da68a465eaf6ab169c836
Chris Mytton <chrismytton@gmail.com>
2015-12-15 18:42:22 +0000

    Place assets in output directory

--- lib/jekyll/search.rb
+++ lib/jekyll/search.rb
@@ -1,7 +1,38 @@
 require 'jekyll/search/version'
 
+require 'jekyll'
+
 module Jekyll
   module Search
-    # Your code goes here...
+    class Generator < ::Jekyll::Generator
+      def generate(site)
+        assets.each do |file|
+          site.static_files << Jekyll::StaticFile.new(
+            site,
+            root_dir,
+            assets_dir,
+            file
+          )
+        end
+      end
+
+      private
+
+      def assets
+        @assets ||= begin
+          Dir.glob(File.join(root_dir, assets_dir, '*')).map do |asset|
+            File.basename(asset)
+          end
+        end
+      end
+
+      def root_dir
+        @root_dir ||= File.expand_path('../../..', __FILE__)
+      end
+
+      def assets_dir
+        @assets_dir ||= File.join('assets', 'jekyll-search')
+      end
+    end
   end
 end
--- test/jekyll/search_test.rb
+++ test/jekyll/search_test.rb
@@ -5,7 +5,12 @@ class Jekyll::SearchTest < Minitest::Test
     refute_nil ::Jekyll::Search::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_generating_assets
+    site = Jekyll::Site.new(Jekyll.configuration)
+    site.generate
+    asset_count = Dir['assets/jekyll-search/*'].size
+    assert_equal asset_count, site.static_files.size
+    asset = site.static_files.first
+    require 'pry'; binding.pry
   end
 end
